ccb4eefe6e6bfef2a47ef7e53a5c9147a4aaff63
Maks Babarowski <maksymilian.babarowski@gmail.com>
2018-05-06 20:01:09 +0200

    Base for parsing options with getopt

--- main.c
+++ main.c
@@ -2,21 +2,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
 
 #include "coder.h"
 
-const char *argp_program_version = "morseconv 0.1";
-static char doc[] = "Encrypts and decrypts message using Morse code. \"-\" stands for long signal, \".\" stands for short signal.";
-static char args_doc[] = "[STRING...] [-v]";
-static struct argp_option options[] = {
-    { "encrypt", 'e', "STRING", 0, "Encrypts string provided and prints the output."},
-    { "decrypt", 'd', "STRING", 0, "Decrypts string provided and prints the plain text."},
-    { 0 }
-};
-
 struct arguments {
     enum {
+        NOT_SET,
         ENCRYPT_MODE,
         DECRYPT_MODE
         } mode;
@@ -24,40 +17,48 @@ struct arguments {
     char* msg;
 };
 
-static error_t parse_opt(int key, char *arg, struct argp_state *state) {
-    struct arguments *arguments = state->input;
-    switch (key) {
-        case 'e':
-        arguments->mode = ENCRYPT_MODE;
-        arguments->msg = arg;
-        break;
+int main( int argc, char* argv[] ) {
+    int c;
+    while( 1 ) {
+        static struct option options[] = {
+            // MODE OPTIONS
+            {"encode", required_argument, 0, 'e' },
+            {"decode", required_argument, 0, 'd' },
+            
+            // ADDITIONAL OPTIONS
+            {"matrix", required_argument, 0, 'm' },
+            {"inspace", required_argument, 0, 's' },
+            {"outspace", required_argument, 0, 'S' },
+            {0, 0, 0, 0}
+        };
 
-        case 'd':
-        arguments->mode = DECRYPT_MODE;
-        arguments->msg = arg;
-        break;
+        int option_index = 0;
 
-        case ARGP_KEY_ARG:
-        return 0;
+        c = getopt_long( argc, argv, "e:d:m:s:S:", options, &option_index );
 
-        default: return ARGP_ERR_UNKNOWN;
+        if( c == -1 ) {
+            break;
+        }
+
+        switch( c ) {
+            case 0:
+            if( options[ option_index ].flag != 0 ) {
+                break;
+            }
+
+            case 'e':
+            arguments.mode ? arguments.mode = ENCRYPT_MODE : exit( 1 );
+            break;
+
+            case 'd':
+            arguments.mode ? arguments.mode = DECRYPT_MODE : exit( 1 );
+            break;
+
+            case 'm':
+            char* user_matrix;
+            strcpy( user_matrix, optarg );
+            
+        }
     }
-    return 0;
-}
-
-static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
-
-int main( int argc, char* argv[] )
-{
-    struct arguments arguments;
-    argp_parse(&argp, argc, argv, 0, 0, &arguments);
-    if( arguments.mode == ENCRYPT_MODE ) {
-        char* out = encode( arguments.msg );
-
-        printf( "%s\n", out );
-    } else if( arguments.mode == DECRYPT_MODE ) {
-        printf( "%s\n", decode( arguments.msg ) );
-    }
-
     return 0;
 }
